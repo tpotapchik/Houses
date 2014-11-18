df7fc83b2d74bb0be532d3e79327a709430126ac
coxa <sahnovski@gmail.com>
2014-11-18 17:25:44 +0300

    db

--- config/db.php
+++ config/db.php
@@ -2,16 +2,16 @@
 if ($_SERVER['HTTP_HOST'] == 'house.loc') {
     return [
         'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-        'username' => 'root',
-        'password' => '',
+        'dsn' => 'pgsql:host=localhost;dbname=home',
+        'username' => 'home',
+        'password' => 'home',
         'charset' => 'utf8',
     ];
 } else {
     //production
     return [
         'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=pgsql1.activeby.net;dbname=domtut',
+        'dsn' => 'pgsql:host=pgsql1.activeby.net;dbname=domtut',
         'username' => 'dom',
         'password' => '6nip2Ujs',
         'charset' => 'utf8',
