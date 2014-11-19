30e5ba8b173065b84cd63fdd03731b1154f83950
coxa <sahnovski@gmail.com>
2014-11-20 01:52:57 +0300

    controller

---
+++ controllers/CatalogController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\FilterPanel;
+use app\models\ProjectSearch;
+use Yii;
+use yii\web\Controller;
+
+class CatalogController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionSearch()
+    {
+        $dataProvider = null;
+        $model = new FilterPanel();
+
+        if ($model->load(Yii::$app->request->get()) && $model->validate()) {
+            $searchModel = new ProjectSearch();
+            $dataProvider = $searchModel->searchFilter($model);
+        } else {
+            //todo remove that
+            var_dump($model->getErrors());
+            die;
+        }
+
+
+        return $this->render('search', ['model' => $model, 'dataProvider' => $dataProvider]);
+    }
+
+    public function actionView()
+    {
+        return $this->render('view');
+    }
+}
+
