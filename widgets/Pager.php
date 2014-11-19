939c37d5e48ee14ed27d0f86ac8db3e4f5b67ec8
coxa <sahnovski@gmail.com>
2014-11-20 01:17:00 +0300

    pager

---
+++ widgets/Pager.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Houses
+ * User: coxa
+ * Date: 20.11.14
+ * Time: 1:00
+ */
+
+namespace app\widgets;
+
+
+use yii\helpers\Html;
+use yii\widgets\LinkPager;
+
+class Pager extends LinkPager
+{
+
+    /**
+     * @var array HTML attributes for the pager container tag.
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
+    public $options = ['class' => 'pagination result clearfix'];
+    /**
+     * @var string the CSS class for the "previous" page button.
+     */
+    public $prevPageCssClass = 'before';
+    /**
+     * @var string the CSS class for the "next" page button.
+     */
+    public $nextPageCssClass = 'after';
+    /**
+     * @var string|boolean the label for the "next" page button. Note that this will NOT be HTML-encoded.
+     * If this property is false, the "next" page button will not be displayed.
+     */
+    public $nextPageLabel = '';
+    /**
+     * @var string|boolean the text label for the previous page button. Note that this will NOT be HTML-encoded.
+     * If this property is false, the "previous" page button will not be displayed.
+     */
+    public $prevPageLabel = '';
+
+    /**
+     * Renders the page buttons.
+     * @return string the rendering result
+     */
+    protected function renderPageButtons()
+    {
+        $pageCount = $this->pagination->getPageCount();
+        if ($pageCount < 2 && $this->hideOnSinglePage) {
+            return '';
+        }
+
+        $buttons = [];
+        $currentPage = $this->pagination->getPage();
+
+        // first page
+        if ($this->firstPageLabel !== false) {
+            $buttons[] = $this->renderPageButton($this->firstPageLabel, 0, $this->firstPageCssClass, $currentPage <= 0, false);
+        }
+
+        // prev page
+        if ($this->prevPageLabel !== false) {
+            if (($page = $currentPage - 1) < 0) {
+                $page = 0;
+            }
+            $buttons[] = $this->renderPageButton($this->prevPageLabel, $page, $this->prevPageCssClass, $currentPage <= 0, false);
+        }
+
+        // internal pages
+        list($beginPage, $endPage) = $this->getPageRange();
+        for ($i = $beginPage; $i <= $endPage; ++$i) {
+            $buttons[] = $this->renderPageButton($i + 1, $i, null, false, $i == $currentPage);
+        }
+
+        // next page
+        if ($this->nextPageLabel !== false) {
+            if (($page = $currentPage + 1) >= $pageCount - 1) {
+                $page = $pageCount - 1;
+            }
+            $buttons[] = $this->renderPageButton($this->nextPageLabel, $page, $this->nextPageCssClass, $currentPage >= $pageCount - 1, false);
+        }
+
+        // last page
+        if ($this->lastPageLabel !== false) {
+            $buttons[] = $this->renderPageButton($this->lastPageLabel, $pageCount - 1, $this->lastPageCssClass, $currentPage >= $pageCount - 1, false);
+        }
+
+        return Html::tag('div', implode("\n", $buttons), $this->options);
+    }
+
+    /**
+     * Renders a page button.
+     * You may override this method to customize the generation of page buttons.
+     * @param string $label the text label for the button
+     * @param integer $page the page number
+     * @param string $class the CSS class for the page button.
+     * @param boolean $disabled whether this page button is disabled
+     * @param boolean $active whether this page button is active
+     * @return string the rendering result
+     */
+    protected function renderPageButton($label, $page, $class, $disabled, $active)
+    {
+        $options = ['class' => $class === '' ? null : $class];
+        if ($active) {
+            Html::addCssClass($options, $this->activePageCssClass);
+        }
+        if ($disabled) {
+            Html::addCssClass($options, $this->disabledPageCssClass);
+
+            return Html::tag('span', $label, $options);
+        }
+        $options['data-page'] = $page;
+
+        return Html::a($label, $this->pagination->createUrl($page), $options);
+    }
+}
