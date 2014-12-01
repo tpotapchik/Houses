4177f31c28b2dbe2e11ee7b583d359eb1f1530c9
coxa <sahnovski@gmail.com>
2014-12-02 00:27:28 +0300

    random main page images

--- widgets/CategoryGrid.php
+++ widgets/CategoryGrid.php
@@ -20,11 +20,12 @@ class CategoryGrid extends ProjectsGrid
         parent::init();
     }
 
-    protected function renderItem(Category $model, $index)
+    protected function renderItem($model, $index)
     {
         /** @var Project $project */
         $odd = $index % 2 == 0 ? 'left' : 'right';
 
+        /** @var Category $model */
         $project = $model->getRandomProject();
 
         $photoLink = $project->getMainPhoto();
