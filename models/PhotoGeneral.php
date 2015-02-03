ca66f02d9e5409c70d27c126ee38e384b7c33836
coxa <sahnovski@gmail.com>
2015-02-04 00:07:09 +0300

    import2

--- models/PhotoGeneral.php
+++ models/PhotoGeneral.php
@@ -20,23 +20,14 @@ class PhotoGeneral extends ActiveRecord
     public function behaviors()
     {
         return [
-            'Photo' => [
+            [
                 'class' => 'app\library\CropPhotoBehavior',
                 'attribute' => 'file',
                 'scenarios' => ['insert', 'update'],
-//                'path' => '@webroot/web',
-//                'url' => '',
                 'ratio' => 1.97,
             ],
 //            [
-//                'class' => CropImageUploadBehavior::className(),
-//                'attribute' => 'file',
-//                'scenarios' => ['insert', 'update'],
-//                'path' => '@webroot/web',
-//                'url' => '',
-//                'ratio' => 1.97,
-////                'crop_field' => 'photo_crop',
-////                'cropped_field' => 'photo_cropped',
+//                'class' => 'app\library\PhotoInsertBehavior',
 //            ],
         ];
     }
