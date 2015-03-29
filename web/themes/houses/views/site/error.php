25299135a791275cb6b1fa877e515f67fcb35461
Танюша <tanyapotapchik@gmail.com>
2015-03-29 19:59:48 +0300

    404 styles 2

--- web/themes/houses/views/site/error.php
+++ web/themes/houses/views/site/error.php
@@ -12,7 +12,6 @@ $this->title = $name;
 <div class="site-error">
 
     <h1>К сожалению, запрошенная вами страница не найдена</h1>
-
     <p>
         Это означает, что страницы, которую вы ищете, больше не существует. <br>
         Возможно, она была удалена, либо вы набрали неправильный адрес.
@@ -20,11 +19,14 @@ $this->title = $name;
     <p>Вы можете перейти на <?= Html::a('главную страницу', ['site/index']); ?> либо в <?= Html::a('каталог проектов', ['catalog/index']) ?>.</p>
     <p>
         Не повезло в поиске - мы вам поможем!
-
     </p>
     <div>
-    <div class="left"><img src="/img/gift.jpg" alt="Получи бесплатную консультацию"></div>
-    <div class="left call_me"><a href="#popup" class="popup">Заказать звонок</a></div>
+    <div class="left" style="margin-left: 180px;">
+        <img src="/img/gift.jpg" class="gift-404" alt="Получи бесплатную консультацию">
+    </div>
+    <div class="left call_me">
+        <p>Если вам сразу не удалось найти то,<br> что искали,<br>не расстаривайтесь!</p>
+        <a href="#popup" class="popup">Заказать звонок</a></div>
     </div>
 
 
