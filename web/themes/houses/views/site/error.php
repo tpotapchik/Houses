a7b831270aad6cf86f415e040e1de2bc7bdffca9
Виктор <niko_viktor@inbox.ru>
2015-04-19 14:24:43 +0200

    nikoviktor

--- web/themes/houses/views/site/error.php
+++ web/themes/houses/views/site/error.php
@@ -12,31 +12,37 @@ $this->title = $name;
 
 <div class="header">
     <div class="centralize">
-        <a href="/" class="logo"><img src="/img/logo3.png" alt="Проектная мастерская Dom-Tut.by"/></a>
+            <a href="/" class="logo"><img src="/img/logo3.png" alt="Проектная мастерская Dom-Tut.by"/></a>
 
-        <div class="call-back right">
-            <div class="social">
-                <a rel="nofollow" href="https://www.youtube.com/channel/UCg34LYTRug0x8hNVbjbeZ3A" target="_blank" class="yt"></a>
-                <a rel="nofollow" href="https://plus.google.com/u/0/108290869583672576270/posts" target="_blank" class="gp"></a>
-                <a rel="nofollow" href="http://vk.com/dom_tut" target="_blank" class="vk"></a>
-                <a rel="nofollow" href="https://www.facebook.com/pages/Проектная-мастерская-Дом-Тутby/1602271246672110" target="_blank" class="fb"></a>
-             </div>
-        </div>
-        <div class="contact right">
-            <a class="mail" href="mailto:info@dom-tut.by"><i class="_ico"></i>info@dom-tut.by</a>
-            <a class="skype" href="skype:dom-tut.by"><i class="_ico"></i>dom-tut.by</a>
-        </div>
-        <div class="phone right">
-            <div class="_ico"></div>
-            <p>
-                <a style="color:#434242; text-decoration: none;" href="tel:+375 17 238 04 05">+375 17 238 04 05</a>
-            </p>
-                        <p>
-                <a style="color:#434242; text-decoration: none;" href="tel:+375 29 328 30 40">+375 29 328 30 40</a>
-            </p>
+            <div class="call-back right">
+                <div class="social">
+                    <a rel="nofollow" href="https://www.youtube.com/channel/UCg34LYTRug0x8hNVbjbeZ3A" target="_blank" class="yt"></a>
+                    <a rel="nofollow" href="https://plus.google.com/u/0/108290869583672576270/posts" target="_blank" class="gp"></a>
+                    <a rel="nofollow" href="http://vk.com/dom_tut" target="_blank" class="vk"></a>
+                    <a rel="nofollow" href="https://www.facebook.com/pages/Проектная-мастерская-Дом-Тутby/1602271246672110" target="_blank" class="fb"></a>
+                 </div>
+                <div class="call_me"><a href="#popup" class="popup">Заказать звонок</a></div>
+            </div>
+            <div class="contact right">
+                <a class="mail" href="mailto:info@dom-tut.by"><i class="_ico"></i>info@dom-tut.by</a>
+                <a class="skype" href="skype:dom-tut.by"><i class="_ico"></i>dom-tut.by</a>
+            </div>
+            <div class="phone right">
+                        <div class="_ico_"></div>
+            <table style="margin-top:-20px;">
+            <tr><td>
+            Работаем без выходных
+                <img src="/img/24-7.jpg" width="80" />
+    </td>
+    <td>
+                   <a style="color:#434242; text-decoration: none;" href="tel:<small>+375 17</small> <b>238 04 05</b>"><small>+375 17</small> <b>238 04 05</b></a>
+                            <br/>
+                    <a style="color:#434242; text-decoration: none;" href="tel:<small>+375 29</small> <b>328 30 40</b>"><small>+375 29</small> <b>328 30 40</b></a>
+                </td></tr>
+                </table>
+            </div>
         </div>
     </div>
-</div>
 
 <div class="header-menu"><div class="centralize clearfix"><a class="header-menu-item active" href="/">Главная</a><a class="header-menu-item" href="/about">О нас</a><a class="header-menu-item" href="/catalog">Каталог проектов</a><a class="header-menu-item" href="/minsk-design-interior">Дизайн интерьеров</a><a class="header-menu-item" href="/news">Новости</a><a class="header-menu-item" href="/contacts">Контакты</a></div></div>
 
@@ -47,11 +53,7 @@ $this->title = $name;
                 <p><small>Это означает, что страницы, которую вы ищете, больше не существует. <br>
                     Возможно, она была удалена, либо вы набрали неправильный адрес.</small></p>
                 <p><img src="http://dom-tut.by/img/error-bg.png"></p>
-                <p>Не повезло в поиске - повезет со скидкой!</p>
-                    <img src="http://dom-tut.by/img/error-bonus.png" width="200">
-                    <br/>
-                    <img src="http://dom-tut.by/img/error-strelki.png" width="200" />
-                <div class="call_me_error">
+            <div class="call_me_error">
                     <a href="#popup" class="popup" style="width:300px">Заказать бесплатный звонок!</a>
         	</div>
     </div>
