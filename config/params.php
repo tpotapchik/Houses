2042d48edd4f301a038a05d0539a91a9e19029fe
coxa <coxa@users.noreply.github.com>
2015-05-25 11:48:54 +0300

    no contacts

--- config/params.php
+++ config/params.php
@@ -8,15 +8,15 @@ return [
             ['label' => 'Каталог проектов', 'url' => ['catalog/index']],
             ['label' => 'Дизайн интерьеров', 'url' => ['design/index']],
             ['label' => 'Новости', 'url' => ['news/index']],
-            ['label' => 'Контакты', 'url' => ['site/contacts']],
+            //['label' => 'Контакты', 'url' => ['site/contacts']],
         ]
     ],
     'contacts' => [
-        'email' => 'info@dom-tut.by',
-        'phone1' => '<small>+375 17</small> <b>238 04 05</b>',
-        'phone2' => '<small>+375 29</small> <b>328 30 40</b>',
-        'skype' => 'dom-tut.by',
-        'address' => '220019, Республика Беларусь,<br>г.Минск, ул. Максима Горецкого, 7-113, 1 этаж (отдельный вход со двора)'
+        //'email' => 'info@dom-tut.by',
+        //'phone1' => '<small>+375 17</small> <b>238 04 05</b>',
+        //'phone2' => '<small>+375 29</small> <b>328 30 40</b>',
+        //'skype' => 'dom-tut.by',
+        //'address' => '220019, Республика Беларусь,<br>г.Минск, ул. Максима Горецкого, 7-113, 1 этаж (отдельный вход со двора)'
     ],
     'social' => [
         'fb' => 'https://www.facebook.com/pages/Проектная-мастерская-Дом-Тутby/1602271246672110',
