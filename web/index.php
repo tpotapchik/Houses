85f007362f9d96045171aab894823f74ba36c0f7
coxa <coxa@users.noreply.github.com>
2015-05-27 16:09:46 +0300

    remove disabled mode

--- web/index.php
+++ web/index.php
@@ -1,9 +1,11 @@
 <?php
+/*
 header('HTTP/1.1 503 Service Temporarily Unavailable');
 header('Status: 503 Service Temporarily Unavailable');
 echo 'Извините, сайт временно не доступен';
 ini_set('display_errors', 'Off');
 throw new \Exception('Service not available', 503);
+*/
 if (getenv('APP_ENV') == 'dev' || $_SERVER['HTTP_HOST'] == 'house.loc') {
     // comment out the following two lines when deployed to production
     defined('YII_DEBUG') or define('YII_DEBUG', true);
