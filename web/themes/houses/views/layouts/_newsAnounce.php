b1786426cb2883e9114913829c37f8be74e0094e
tpotapchik <tanyapotapchik@gmail.com>
2017-07-02 16:39:42 +0300

    maim page layout

--- web/themes/houses/views/layouts/_newsAnounce.php
+++ web/themes/houses/views/layouts/_newsAnounce.php
@@ -12,18 +12,25 @@ use yii\helpers\Html;
 //todo what about cache this query?
 //get 5th last news
 $articles = Article::find()->limit(5)->orderBy(['created_at' => SORT_DESC])->andWhere(['category_id' => 2,
-    'is_published' => true])->all();
+'is_published' => true])->all();
 
 ?>
 <div class="list-news">
-    <h2>Новости</h2>
-    <ul class="list-news-content">
-        <?php
-        /** @var Article $article */
-        foreach ($articles as $article) {
-                echo Html::tag('li', Html::a($article->title, ['news/'.$article->url_key]));
-        }
-        ?>
-    </ul>
-    <span class="square"></span>
+	<h2>Новости</h2>
+	<ul class="list-news__content">
+		<li class="list-news__item-wrap">
+			<a class="list-news__item" href="#">
+				<img src="" alt=""/>
+				<div>Зимнее строительство: плюсы и особенности</div>
+			</a>
+		</li>
+
+		<li class="list-news__item-wrap">
+			<a class="list-news__item" href="#">
+				<img src="" alt=""/>
+				<div>Как подобрать проект?</div>
+			</a>
+		</li>
+	</ul>
+	<span class="square"></span>
 </div>
--- web/themes/houses/views/site/index.php
+++ web/themes/houses/views/site/index.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
 <?php
 /* @var $this yii\web\View */
 use app\models\Article;
@@ -8,12 +15,10 @@ $mainArticle = Article::getMain();
 $this->registerMetaTag(['name' => 'keywords', 'content' => $mainArticle->meta_keywords]);
 $this->registerMetaTag(['name' => 'description', 'content' => $mainArticle->meta_description]);
 ?>
-
 <div class="slider-content">
 	<?= $this->render('../layouts/_filter-panel', ['other' => false]) ?>
 	<div class="slider">
 		<div class="slider-item" style="background-image: url('http://1house.by/wp-content/uploads/2017/01/Perspektiva-12.jpg')">
-
 			<a href="/catalog/odnosemeynyy_odnoetajnyy/ariel" class="slider-link">
 				<div class="slider-text">
 					<div class="centralize clearfix">
@@ -25,7 +30,6 @@ $this->registerMetaTag(['name' => 'description', 'content' => $mainArticle->meta
 				</div>
 			</a>
 		</div>
-
 		<div class="slider-item" style="background-image: url('http://1house.by/wp-content/uploads/2017/01/Perspektiva-12.jpg')">
 			<a href="/catalog/odnosemeynyy_odnoetajnyy/decyma" class="slider-link">
 				<div class="slider-text">
@@ -38,13 +42,13 @@ $this->registerMetaTag(['name' => 'description', 'content' => $mainArticle->meta
 				</div>
 			</a>
 		</div>
-
 		<div class="slider-item" style="background-image: url('/img/temp/slider3.jpg')">
 			<a href="/catalog/odnosemeynyy_odnoetajnyy/ambrozja" class="slider-link">
 				<div class="slider-text">
 					<div class="centralize clearfix">
 						<div class="_title">AMBROZJA</div>
-						<div class="_content ovhidden">Одноэтажный современный дом с гаражом на 2 автомобиля. <br>
+						<div class="_content ovhidden">Одноэтажный современный дом с гаражом на 2 автомобиля.
+							<br>
 							И еще одна строчка
 						</div>
 					</div>
@@ -57,7 +61,8 @@ $this->registerMetaTag(['name' => 'description', 'content' => $mainArticle->meta
 					<div class="centralize clearfix">
 						<div class="_title">AMBROZJA 3</div>
 						<div class="_content ovhidden">Современный и просторный одноэтажный дом с гаражом для 2
-							автомобилей. <br>И еще одна строчка
+							автомобилей.
+							<br>И еще одна строчка
 						</div>
 					</div>
 				</div>
@@ -68,19 +73,18 @@ $this->registerMetaTag(['name' => 'description', 'content' => $mainArticle->meta
 				<div class="slider-text">
 					<div class="centralize">
 						<div class="_title">JEŻYNA</div>
-						<div class="_content ovhidden">Опрятный односемейный дом с мансардой. <br> И еще одна строчка
+						<div class="_content ovhidden">Опрятный односемейный дом с мансардой.
+							<br> И еще одна строчка
 						</div>
 					</div>
 				</div>
 			</a>
 		</div>
 	</div>
-
 	<!--main part-->
 	<div class="centralize">
 		<div class="main-title">Проекты домов DOBRY DOM</div>
 	</div>
-
 	<!--tabs-->
 	<div class="tabs-panel-content">
 		<div class="centralize">
@@ -97,26 +101,33 @@ $this->registerMetaTag(['name' => 'description', 'content' => $mainArticle->meta
 		</div>
 	</div>
 	<div class="centralize">
-
 		<!--part with sidebar-->
 		<div class="layout-sidebar">
 			<div class="sidebar-wrap">
 				<div class="sidebar">
 					<ul>
-						<li class="sidebar-item"><a href="#"><span>Проекты</span></a></li>
-						<li class="sidebar-item"><a href="#"><span>Как купить проект дома?</span></a></li>
-						<li class="sidebar-item"><a href="#"><span>Что включает проект?</span></a></li>
-						<li class="sidebar-item"><a href="#"><span>Ландшафтный дизайн</span></a></li>
+						<li class="sidebar-item">
+							<a href="#"><span>Проекты</span></a>
+						</li>
+						<li class="sidebar-item">
+							<a href="#"><span>Как купить проект дома?</span></a>
+						</li>
+						<li class="sidebar-item">
+							<a href="#"><span>Что включает проект?</span></a>
+						</li>
+						<li class="sidebar-item">
+							<a href="#"><span>Ландшафтный дизайн</span></a>
+						</li>
 					</ul>
 				</div>
-
 				<div class="sidebar">
 					<div class="feedback-form">
 						<form method="POST" action="/">
 							<input type="hidden" name="" id="sessid" value="">
 							<input type="hidden" name="" value="">
-
-							<div class="form-title">Будьте в курсе <span>событий!</span></div>
+							<div class="form-title">Будьте в курсе
+								<span>событий!</span>
+							</div>
 							<div class="form">
 								<button type="submit">+</button>
 								<div>
@@ -131,168 +142,157 @@ $this->registerMetaTag(['name' => 'description', 'content' => $mainArticle->meta
 				<div class="right-block-content clearfix">
 					<div class="_content">
 						<a href="project-house.html" class="picture-block">
-
-							<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
-
+							<div class="picture-block__img">
+								<img src="http://dom-tut.by/images/060/574/4dc/77d6c292414298b548165ae-492x369.jpg" alt="Картинка дома"/>
+							</div>
 							<div class="info-block">
 								<div class="_title-project">Aplauz 2 / Аплауш 2
-									<div class="effectiveArea">150 м<sup>2</sup></div>
+									<div class="effectiveArea">150 м<sup>2</sup>
+									</div>
 								</div>
-
 							</div>
 						</a>
 					</div>
 					<div class="_content">
 						<a href="project-house.html" class="picture-block">
-
-							<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
-
+							<div class="picture-block__img">
+								<img src="http://dom-tut.by/images/a47/ac5/bef/e08f34f8bd520c1a8a5c505-492x369.jpg" alt="Картинка дома"/>
+							</div>
 							<div class="info-block">
 								<div class="_title-project">Aplauz 2 / Аплауш 2
-									<div class="effectiveArea">150 м<sup>2</sup></div>
+									<div class="effectiveArea">150 м<sup>2</sup>
+									</div>
 								</div>
-
 							</div>
 						</a>
 					</div>
 					<div class="_content">
 						<a href="project-house.html" class="picture-block">
-
-							<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
-
+							<div class="picture-block__img">
+								<img src="http://dom-tut.by/images/b8c/69a/00e/d7a48ea84c56115bdb146c9-492x369.jpg" alt="Картинка дома"/>
+							</div>
 							<div class="info-block">
 								<div class="_title-project">Aplauz 2 / Аплауш 2
-									<div class="effectiveArea">150 м<sup>2</sup></div>
+									<div class="effectiveArea">150 м<sup>2</sup>
+									</div>
 								</div>
-
 							</div>
 						</a>
 					</div>
 					<div class="_content">
 						<a href="project-house.html" class="picture-block">
-
-							<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
-
+							<div class="picture-block__img">
+								<img src="http://dom-tut.by/images/a47/ac5/bef/e08f34f8bd520c1a8a5c505-492x369.jpg" alt="Картинка дома"/>
+							</div>
 							<div class="info-block">
 								<div class="_title-project">Aplauz 2 / Аплауш 2
-									<div class="effectiveArea">150 м<sup>2</sup></div>
+									<div class="effectiveArea">150 м<sup>2</sup>
+									</div>
 								</div>
-
 							</div>
 						</a>
 					</div>
 					<div class="_content">
 						<a href="project-house.html" class="picture-block">
-
-							<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
-
+							<div class="picture-block__img">
+								<img src="http://dom-tut.by/images/60d/62f/74c/87143f73fac94074331fae8-492x369.jpg" alt="Картинка дома"/>
+							</div>
 							<div class="info-block">
 								<div class="_title-project">Aplauz 2 / Аплауш 2
-									<div class="effectiveArea">150 м<sup>2</sup></div>
+									<div class="effectiveArea">150 м<sup>2</sup>
+									</div>
 								</div>
-
 							</div>
 						</a>
 					</div>
 					<div class="_content">
 						<a href="project-house.html" class="picture-block">
-
-							<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
-
+							<div class="picture-block__img">
+								<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
+							</div>
 							<div class="info-block">
 								<div class="_title-project">Aplauz 2 / Аплауш 2
-									<div class="effectiveArea">150 м<sup>2</sup></div>
+									<div class="effectiveArea">150 м<sup>2</sup>
+									</div>
 								</div>
-
 							</div>
 						</a>
 					</div>
 					<div class="_content">
 						<a href="project-house.html" class="picture-block">
-
-							<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
-
+							<div class="picture-block__img">
+								<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
+							</div>
 							<div class="info-block">
 								<div class="_title-project">Aplauz 2 / Аплауш 2
-									<div class="effectiveArea">150 м<sup>2</sup></div>
+									<div class="effectiveArea">150 м<sup>2</sup>
+									</div>
 								</div>
-
 							</div>
 						</a>
 					</div>
 					<div class="_content">
 						<a href="project-house.html" class="picture-block">
-
-							<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
-
+							<div class="picture-block__img">
+								<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
+							</div>
 							<div class="info-block">
 								<div class="_title-project">Aplauz 2 / Аплауш 2
-									<div class="effectiveArea">150 м<sup>2</sup></div>
+									<div class="effectiveArea">150 м<sup>2</sup>
+									</div>
 								</div>
-
 							</div>
 						</a>
 					</div>
 					<div class="_content">
 						<a href="project-house.html" class="picture-block">
-
-							<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
-
+							<div class="picture-block__img">
+								<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
+							</div>
 							<div class="info-block">
 								<div class="_title-project">Aplauz 2 / Аплауш 2
-									<div class="effectiveArea">150 м<sup>2</sup></div>
+									<div class="effectiveArea">150 м<sup>2</sup>
+									</div>
 								</div>
-
 							</div>
 						</a>
 					</div>
 					<div class="_content">
 						<a href="project-house.html" class="picture-block">
-
-							<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
-
+							<div class="picture-block__img">
+								<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
+							</div>
 							<div class="info-block">
 								<div class="_title-project">Aplauz 2 / Аплауш 2
-									<div class="effectiveArea">150 м<sup>2</sup></div>
+									<div class="effectiveArea">150 м<sup>2</sup>
+									</div>
 								</div>
-
 							</div>
 						</a>
 					</div>
 					<div class="_content">
 						<a href="project-house.html" class="picture-block">
-
-							<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
-
+							<div class="picture-block__img">
+								<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
+							</div>
 							<div class="info-block">
 								<div class="_title-project">Aplauz 2 / Аплауш 2
-									<div class="effectiveArea">150 м<sup>2</sup></div>
+									<div class="effectiveArea">150 м<sup>2</sup>
+									</div>
 								</div>
-
 							</div>
 						</a>
 					</div>
 					<div class="_content">
-						<a href="project-house.html" class="picture-block">
-
-							<img src="img/temp/house-new.jpg" alt="Картинка дома"/>
-
-							<div class="info-block">
-								<div class="_title-project">Aplauz 2 / Аплауш 2
-									<div class="effectiveArea">150 м<sup>2</sup></div>
-								</div>
-
-							</div>
+						<a href="project-house.html" class="picture-block picture-block--promo">
+							<span class="_text">Показать все</span>
 						</a>
 					</div>
 				</div>
 			</div>
 		</div>
-
 		<!--/part with sidebar-->
-
 		<div class="main-title">Наши преимущества</div>
-
 		<div class="slider-to-start">
 			<div class="slider-to-start-item">
 				<a href="/catalog/odnosemeynyy_odnoetajnyy/ariel_KO" class="slider-to-start-link">
@@ -315,12 +315,12 @@ $this->registerMetaTag(['name' => 'description', 'content' => $mainArticle->meta
                        </svg>
 					</div>
 					<div class="slider-to-start-text">
-						Опыт свыше <br>
+						Опыт свыше
+						<br>
 						10 лет
 					</div>
 				</a>
 			</div>
-
 			<div class="slider-to-start-item">
 				<a href="/catalog/odnosemeynyy_odnoetajnyy/ariel_KO" class="slider-to-start-link">
 					<div class="ing-content">
@@ -339,11 +339,11 @@ $this->registerMetaTag(['name' => 'description', 'content' => $mainArticle->meta
                         </svg>
 					</div>
 					<div class="slider-to-start-text">
-						Комплексный <br>подход
+						Комплексный
+						<br>подход
 					</div>
 				</a>
 			</div>
-
 			<div class="slider-to-start-item">
 				<a href="/catalog/odnosemeynyy_odnoetajnyy/ariel_KO" class="slider-to-start-link">
 					<div class="ing-content">
@@ -365,11 +365,11 @@ $this->registerMetaTag(['name' => 'description', 'content' => $mainArticle->meta
                        </svg>
 					</div>
 					<div class="slider-to-start-text">
-						Авторский <br>надзор
+						Авторский
+						<br>надзор
 					</div>
 				</a>
 			</div>
-
 			<div class="slider-to-start-item">
 				<a href="/catalog/odnosemeynyy_odnoetajnyy/ariel_KO" class="slider-to-start-link">
 					<div class="ing-content">
@@ -391,11 +391,11 @@ $this->registerMetaTag(['name' => 'description', 'content' => $mainArticle->meta
                         </svg>
 					</div>
 					<div class="slider-to-start-text">
-						Всё <br>официально
+						Всё
+						<br>официально
 					</div>
 				</a>
 			</div>
-			
 			<div class="slider-to-start-item">
 				<a href="/catalog/odnosemeynyy_odnoetajnyy/ariel_KO" class="slider-to-start-link">
 					<div class="ing-content">
@@ -415,11 +415,12 @@ $this->registerMetaTag(['name' => 'description', 'content' => $mainArticle->meta
                         </svg>
 					</div>
 					<div class="slider-to-start-text">
-						Индивидуальный <br>подход к каждому <br>клиенту
+						Индивидуальный
+						<br>подход к каждому
+						<br>клиенту
 					</div>
 				</a>
 			</div>
-			
 			<div class="slider-to-start-item">
 				<a href="/catalog/odnosemeynyy_odnoetajnyy/ariel_KO" class="slider-to-start-link">
 					<div class="ing-content">
@@ -452,11 +453,12 @@ viewBox="0 0 799 648"
 </svg>
 					</div>
 					<div class="slider-to-start-text">
-						100% проектов <br>проходят <br>согласование
+						100% проектов
+						<br>проходят
+						<br>согласование
 					</div>
 				</a>
 			</div>
-			
 			<div class="slider-to-start-item">
 				<a href="/catalog/odnosemeynyy_odnoetajnyy/ariel_KO" class="slider-to-start-link">
 					<div class="ing-content">
@@ -522,18 +524,16 @@ viewBox="0 0 1025 574"
 </svg>
 					</div>
 					<div class="slider-to-start-text">
-						3D визуализация <br>проекта <br>
+						3D визуализация
+						<br>проекта
+						<br>
 					</div>
 				</a>
 			</div>
 		</div>
-
 	</div>
 </div>
-
-
 <!--Parallax-->
-
 <div class="parallax-window" data-parallax="scroll" data-image-src="/img/temp/parallax-bg.png">
 	<div class="parallax-window-container">
 		<div class="shadow"></div>
@@ -545,46 +545,46 @@ viewBox="0 0 1025 574"
 	</div>
 </div>
 <!--/Parallax-->
-
-
 <div class="centralize">
-
 	<!--<?= $this->render('../layouts/_popular', []) ?>-->
 	<!-do later and _delete html below-->
-
+	<div class="main-title">Интерьеры популярных проектов</div>
 	<div class="main-block-interior clearfix">
+		<a class="interior-block" href="/catalog/odnosemeynyy_s_jiloy_mansardoy/lara_3-foto.html">
+			<img src="http://dom-tut.by/images/c0b/fed/3010d4783fcd9abbeeb00ebcb7-327x206.jpg" alt=""></a>
+		<a class="interior-block" href="/catalog/odnosemeynyy_s_jiloy_mansardoy/lara_3-foto.html">
+			<img src="http://dom-tut.by/images/c0b/fed/3010d4783fcd9abbeeb00ebcb7-327x206.jpg" alt=""></a>
+		<a class="interior-block" href="/catalog/odnosemeynyy_s_jiloy_mansardoy/lara_3-foto.html">
+			<img src="http://dom-tut.by/images/c0a/9d5/a0cdb374be0c58f5fb930fe348-327x206.jpg" alt=""></a>
+		<a class="interior-block" href="/catalog/odnosemeynyy_s_jiloy_mansardoy/lara_3-foto.html">
+			<img src="http://dom-tut.by/images/0b6/83f/5bcfe42801d5cd4fdc056d4a21-327x206.jpg" alt=""></a>
+		<a class="interior-block" href="/catalog/odnosemeynyy_s_jiloy_mansardoy/lara_3-foto.html"><img src="/images/7a1/d33/bb59447ecc033cc00129837e7e-327x206.jpg" alt=""></a>
+		<a class="interior-block" href="/catalog/odnosemeynyy_s_jiloy_mansardoy/lara_3-foto.html"><img src="/images/7a1/d33/bb59447ecc033cc00129837e7e-327x206.jpg" alt=""></a>
+		<a class="interior-block" href="/catalog/odnosemeynyy_s_jiloy_mansardoy/lara_3-foto.html"><img src="/images/7a1/d33/bb59447ecc033cc00129837e7e-327x206.jpg" alt=""></a>
 		<a class="interior-block" href="/catalog/odnosemeynyy_s_jiloy_mansardoy/lara_3-foto.html"><img src="/images/7a1/d33/bb59447ecc033cc00129837e7e-327x206.jpg" alt=""></a>
 	</div>
-
-
 	<?= $this->render('../layouts/_favorite', []) ?>
-
 	<div class="common-block clearfix">
 		<div class="left-block">
 			<!--<?= $this->render('../layouts/_newsAnounce', []) ?>
 			<?= $this->render('../layouts/_right-menu', []) ?>-->
-
 			<!--место для баннеров-->
 			<div class="banners-content clearfix">
 				<div>Место для баннера 1</div>
 				<div>Место для баннера 2</div>
 			</div>
-
 		</div>
 		<div class="main-text-block ovhidden">
 			<?= $this->render('../layouts/_newsAnounce', []) ?>
 			<?= $mainArticle->full_text ?>
 		</div>
 	</div>
-
-
 	<!--<?= $this->render('../layouts/_our-projects', []) ?>
 
 	<?= $this->render('../layouts/_we-suggest', []) ?>-->
-
 	<div>
 		<?= $this->render('../layouts/_parthners', []) ?>
 	</div>
-
 </div>
-
+</body>
+</html>
